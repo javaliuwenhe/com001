03aca34d3327fd33db28599b844c92964b63e24a
javaliuwenhe <50253622+javaliuwenhe@users.noreply.github.com>
2019-05-04 09:08:06 +0800

    Delete HelloWorld01.java

--- HelloWorld01.java
+++
@@ -1,9 +0,0 @@
-package com.kgc.kgc0201;
-/*Hello.java
-2015-10-9
-v.1.0*/
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hello\t world£¡£¡£¿\t!\n!");
-    }
-}
